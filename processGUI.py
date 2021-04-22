81aa6b57b744316b60e025058359d7313190a58d
lenhatquang97 <19120340@student.hcmus.edu.vn>
2021-04-22 14:29:42 +0700

    GUI process

--- processGUI.py
+++ processGUI.py
@@ -5,7 +5,7 @@ import socket
 # pip install pillow
 from PIL import Image, ImageTk
 from pyautogui import scroll
-class KeyloggerWindow(Frame):
+class Process(Frame):
     def __init__(self,master,ip,port_no):
         Frame.__init__(self, master)
         self.master=master
@@ -13,35 +13,50 @@ class KeyloggerWindow(Frame):
         self.ip=ip
         self.port_no=port_no
     
-        hookButton = ttk.Button(self.master, text='Hook',command=self.manageEventHook)
-        hookButton.place(x=5,y=5,height=60)
+        killButton = ttk.Button(self.master, text='Kill',command=self.eventKillProcess)
+        killButton.place(x=5,y=5,height=60)
 
-        unHookButton = ttk.Button(self.master, text='Unhook',command=self.eventUnhook)
-        unHookButton.place(x=130,y=5,height=60)
+        watchButton = ttk.Button(self.master, text='Watch',command=self.eventWatchProcess)
+        watchButton.place(x=130,y=5,height=60)
         
-        printButton = ttk.Button(self.master, text='Print',command=self.eventPrint)
-        printButton.place(x=255,y=5,height=60)
+        deleteButton = ttk.Button(self.master, text='Delete',command=self.eventDeleteProcess)
+        deleteButton.place(x=255,y=5,height=60)
 
-        deleteButton = ttk.Button(self.master, text='Delete',command=self.eventDelete)
-        deleteButton.place(x=380,y=5,height=60)
+        startButton = ttk.Button(self.master, text='Start',command=self.eventStartProcess)
+        startButton.place(x=380,y=5,height=60)
         #file status
 
         self.treeViewProcess=ttk.Treeview(self.master)
-        self.treeViewProcess["columns"]=("one","two","three")
+        s = ttk.Style()
+        s.configure('Treeview', rowheight=30)
+        
+        self.treeViewProcess["columns"]=("one","two")
+        self.treeViewProcess.column("#0",width=165,anchor=CENTER)
+        self.treeViewProcess.column("one",width=165,anchor=CENTER)
+        self.treeViewProcess.column("two",width=165,anchor=CENTER)
+        self.treeViewProcess.heading("#0",text='Name Process')
+        self.treeViewProcess.heading("one",text='ID Process')
+        self.treeViewProcess.heading("two",text='Count Threads')
+
+        #Mau
+        self.treeViewProcess.insert("",'end',text='notepad.exe',values=("1234",'12'))
+
+        self.treeViewProcess.place(x=5, y=80,height=200)
+
         
 
 
     def loadKeyLog(self):
-        self.master.wm_title("Keylogger")
-        self.master.geometry('510x250')
+        self.master.wm_title("Process")
+        self.master.geometry('510x300')
         self.master.mainloop()
-    def manageEventHook(self):
-        threading.Thread(target=self.eventHook).start()
-    def eventHook(self):
+    def eventKillProcess(self):
         pass
-    def eventUnhook(self):
+    def eventWatchProcess(self):
         pass
-    def eventPrint(self):
+    def eventDeleteProcess(self):
         pass 
-    def eventDelete(self):
+    def eventStartProcess(self):
         pass
+ins = Process(Tk(),'192.168.137.1',1025)
+ins.loadKeyLog()
